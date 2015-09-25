0817357d96ec93bfae0b0500fd8db7cfefea49ed
Anthony Dubis <anthonydubis@gmail.com>
2015-09-25 11:00:43 -0400

    Now directly determining term frequencies for each set of documents (relevant/irrelevant) rather than mapping them individually only to later combine them.

--- src/bingboost/BingBoost.java
+++ src/bingboost/BingBoost.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,16 +11,16 @@ public class BingBoost {
 	// Initial parameters
 	String origQuery;
 	Result[] results;
+	int n_relevant_terms;
+	int n_irrelevant_terms;
 	Map<String, Boolean> stopwords;
 	
 	// Normalized frequency for terms
-	ArrayList<Map<String, Float>> matches;
-	ArrayList<Map<String, Float>> misses;
+	Map<String, Float> matches;
+	Map<String, Float> misses;
 	
 	public BingBoost() throws FileNotFoundException, IOException {
 		stopwords = readStopwords();
-		matches = new ArrayList<Map<String, Float>>();
-		misses = new ArrayList<Map<String, Float>>();
 	}
 	
 	/*
@@ -42,43 +41,81 @@ public class BingBoost {
 		return map;
 	}
 	
-	private Map<String, Float> normalizedMap(Result r) {
-		int n = 0;
-		Map<String, Float> map = new HashMap<String, Float>();
+	/*
+	 * Add the terms in the result to the correct map (matches/misses) based on
+	 * whether the Result is relevant or not. Keep track of the number of terms going
+	 * into each.
+	 */
+	private void mapTermCounts(Result r) {
+		Map<String, Float> map = (r.relevant > 0) ? matches : misses;
 
 		// Build up map with absolute counts
 		for (String s : r.description.split("\\W+")) {
 			if (stopwords.containsKey(s))
 				continue;
 			
-			n++;
+			// Update total number of relevant or irrelevant words
+			if (r.relevant > 0) 
+				n_relevant_terms++; 
+			else 
+				n_irrelevant_terms++;
+			
 			if (map.get(s) != null)
 				map.put(s, map.get(s) + 1);
 			else
 				map.put(s, 1f);
 		}
-		
-		// Normalize map counts
-		for (String s : map.keySet()) 
-			map.put(s, map.get(s) / n);
-		
-		return map;
 	}
 	
+	/*
+	 * This function will set the matches and misses maps to contain average frequencies
+	 * for each word over each set of documents (relevant and irrelevant). As an example, if
+	 * there are two relevant documents, the absolute count that non-stop words appear in
+	 * either will be obtained. The absolute count for each term will then be normalized by
+	 * dividing it by the total number of non-stop words in each of the two documents.
+	 */
 	private void createNormalizedMaps() {
 		for (Result r : results) {
-			Map<String, Float> map = normalizedMap(r);
-			if (r.relevant > 0)
-				matches.add(map);
-			else
-				misses.add(map);
+			mapTermCounts(r);
 		}
+		
+		if (n_relevant_terms > 0) {
+			for (String s : matches.keySet())
+				matches.put(s, matches.get(s) / n_relevant_terms);
+		}
+		
+		if (n_irrelevant_terms > 0) {
+			for (String s : misses.keySet())
+				misses.put(s, misses.get(s) / n_irrelevant_terms);
+		}
+		
+		// Print out the frequencies for testing purposes
+		System.out.println("RELEVANT TERM FREQUENCIES");
+		for (String s : matches.keySet())
+			System.out.println(s + " : " + matches.get(s));
+		
+		System.out.println("IRRELEVANT TERM FREQUENCIES");
+		for (String s : misses.keySet())
+			System.out.println(s + " : " + misses.get(s));
 	}
 	
+	/*
+	 * Outside of the constructor, the only public function for this class. Should reset
+	 * all instance variable to process the new query and results.
+	 * 
+	 * Avoid reconstructing this object as you have to read in the stopwords each time.
+	 */
 	public String updatedQueryForFeedback(String query, Result[] results) {
+		// Initialize instance variables
+		n_relevant_terms = n_irrelevant_terms = 0;
+		matches = new HashMap<String, Float>();
+		misses = new HashMap<String, Float>();
 		this.origQuery = query;
 		this.results = results;
+		
+		// Process the results
 		createNormalizedMaps();
+		
 		return "";
 	}
 
